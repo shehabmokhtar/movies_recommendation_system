e4f8f393900cc290f804a847780ec40f23d3c876
Shehab Mokhtar <shahbeldinmokhtar@gmail.com>
2024-04-13 20:41:44 +0300

    Work with panads

---
+++ main.py
@@ -0,0 +1,10 @@
+# Importing libararies and packages
+import pandas as pd
+
+
+
+# Importing the dataset
+dataset = pd.read_csv('./movies_dataset.csv')
+
+# Print the first 5 rows in the dataset
+print(dataset.head())
